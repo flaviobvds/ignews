67bf7b7b26ebabb2ee3b60c893db02d44332edef
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-01-08 17:03:25 -0400

    Authentication with Next JS

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,14 +1,14 @@
 import Stripe from 'stripe'
-import { version } from '../../package.json'
+//import { version } from '../../package.json'
 
 export const stripe = new Stripe(
-    //process.env.STRIPE_API_KEY,
-    'sk_test_51MNjpCBLj9CO0LTdN8r6pkDyWqD4EYMKWK5GBmBS2rGiJF9iXp6mq4Tiw3usJjI0AzdJxi5dCD1ANl4F7YjyE0yS005n2LVMx5',
+    process.env.STRIPE_API_KEY!,
+    //'sk_test_51MNjpCBLj9CO0LTdN8r6pkDyWqD4EYMKWK5GBmBS2rGiJF9iXp6mq4Tiw3usJjI0AzdJxi5dCD1ANl4F7YjyE0yS005n2LVMx5',
     {
         apiVersion: '2022-11-15',
         appInfo: {
             name: 'ig.news',
-            version
+            version: '0.1.0'
         }
     }
 )
